96a81280d72905f2a42aad81dd4b2a534def22e2
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-11-14 10:34:47 +0100

    ex01

---
+++ ex01/include/Fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int _fixedPoint;
+		static const int _bits;
+
+	public:
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed& src);
+		Fixed& operator=(const Fixed& src);
+		~Fixed();
+
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
+
+std::ostream &operator<<(std::ostream& out, const Fixed &fixed);
+
+#endif
